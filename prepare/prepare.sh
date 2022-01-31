b4c38f591264175a64d5267d4378b395b8a34355
Takuya Niita <takuya2230@gmail.com>
2022-01-31 10:21:44 +0900

    スクリプト修正

--- prepare/prepare.sh
+++ prepare/prepare.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-
 #Get CompartmentID
-oci iam compartment list | grep compartment-id > compartment-id.txt ; cut -f 2 -d ":" compartment-id.txt | tr -d ' ','"',',' | tee compartment-id-tee.txt &>/dev/null ; compartment_id=`cat ./compartment-id-tee.txt` ; rm -rf ./compartment-id* ; echo ${compartment_id}
+oci iam compartment list | grep compartment-id | head -n 1 > compartment-id.txt ; cut -f 2 -d ":" compartment-id.txt | tr -d ' ','"',',' | tee compartment-id-tee.txt &>/dev/null ; compartment_id=`cat ./compartment-id-tee.txt` ; rm -rf ./compartment-id* ; echo ${compartment_id}
 
 #Create Dynamic-Group
 oci iam dynamic-group create --name OCI_DevOps_Dynamic_Group --description OCI_DevOps_Dynamic_Group --matching-rule "any {resource.compartment.id = '${compartment_id}',instance.compartment.id = '${compartment_id}'}"
